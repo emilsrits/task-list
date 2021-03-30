ee85354a69bb7907b5b69b4fec0368cff85cb019
Emils Rits <15720299+emilsrits@users.noreply.github.com>
2021-03-30 18:01:24 +0300

    Add markdown style of formatting for links in task description

--- src/js/components/Task/TaskListItem.vue
+++ src/js/components/Task/TaskListItem.vue
@@ -98,7 +98,15 @@ export default {
         },
 
         descriptionFormatted() {
-            return this.task.description.replace(/((https|http)?:\/\/[^\s]+)/g, '<a href="$1" target="_blank">$1</a>');
+            const linkRegex = /\[(.+)\]\(((https|http)?:\/\/[^\s]+)\)|((https|http)?:\/\/[^\s]+)/gi;
+            
+            return this.task.description.replace(linkRegex, (match, p1, p2) => {
+                if (match && (p1 && p2)) {
+                    return `<a href="${p2}" target="_blank">${p1}</a>`;
+                } else if (match) {
+                    return `<a href="${match}" target="_blank">${match}</a>`;
+                }
+            });
         },
 
         styleColorLabel() {
